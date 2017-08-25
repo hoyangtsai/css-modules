b529adad1f56bd9998056bd07762ced6e3e0d85c
Ho-Yang Tsai <hoyang.t@gmail.com>
2017-08-25 11:00:15 +0800

    Add slice images

--- config/pageConfig.js
+++ config/pageConfig.js
@@ -7,6 +7,40 @@ let browsersList = [
   'iOS>=7'
 ];
 
+let spriteOpts = {
+  stylesheetPath: './client/style',
+  spritePath: './client/slice/',
+  relativeTo: 'rule',
+  filterBy: (image) => {
+    if (/image/i.test(image.url)) return Promise.reject();
+    return Promise.resolve(image);
+  },
+  groupBy: (image) => {
+    const groups = image.url.split('/', -1);
+    if (groups.length < 3) return Promise.reject();
+    let groupName = '';
+    if (groups.length > 4) {
+      groupName = groups[groups.length - 3] + '.' + groups[groups.length - 2];
+    } else {
+      groupName = groups[groups.length - 2];
+    }
+    return Promise.resolve(groupName);
+  },
+  hooks: {
+    //更新生成后的规则，这里主要是改变了生成后的url访问路径
+    onUpdateRule: function (rule, token, image) {
+      let rel = path.relative(
+        path.dirname(image.styleFilePath),
+        path.resolve(__dirname, './src')
+      );
+      rel = rel.replace(/\\+/g, '\/');
+      const spriteUrl = /\/sprite\/sprite.*/.exec(image.spriteUrl);
+      image.spriteUrl = rel + spriteUrl;
+      sprites.updateRule(rule, comment, image);
+    }
+  }
+}
+
 module.exports = {
   path: "", //页面层级
   entry: ["index"], //页面文件列表 Array or Object
@@ -40,7 +74,8 @@ module.exports = {
           options: {
             plugins: (loader) => [
               require('postcss-cssnext')({ browsersList }),
-              require('precss')()
+              require('precss')(),
+              require('postcss-sprites')
             ]
           }
         }
